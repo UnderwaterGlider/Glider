4146963c278c4c43c9c45ca2d9ac02239a2e4194
andreysher <andreysher97@gmail.com>
2017-08-30 10:13:14 +0700

    bolee novaya versiya dvishkov

---
+++ firmware/firmware_1.ino
@@ -0,0 +1,299 @@
+#include <Servo.h>
+#include <EEPROM.h>
+
+#define PISTON_STEP_PIN 2
+#define PISTON_ENABLE_PIN 4
+#define PISTON_DIR_PIN 3
+#define MASS_STEP_PIN 5
+#define MASS_ENABLE_PIN 7
+#define MASS_DIR_PIN 6
+
+//Location we want the data to be put. (global variable)
+int eeAddressPiston = 0; // Piston
+int eeAddressMass = eeAddressPiston + sizeof(int); // Mass
+int maxTurn1 = 0;
+int maxTurn2 = 0;
+
+// Engine class (for three engines, class methods get angles in degrees and full speed divider)
+class engine
+{
+public:
+// Constructor
+engine(int stepP, int enableP, int dirP, int frontP = -1, int backP = -1, int frontPM = -1, int backPM = -1)
+: StepPin (stepP), EnablePin (enableP), DirectionPin(dirP), FrontPin(frontP), BackPin(backP), FrontPinMode(frontPM), BackPinMode(backPM)
+{
+// cout « "Engine addition" « endl;
+stepEngine = 0;
+speedDivider = 1;
+turn = 0;
+switcher = 0;
+TurnMax = 0;
+TurnMin = 0;
+sD = 0;
+}
+
+void installization()
+{
+pinMode(EnablePin, OUTPUT); //Enable
+pinMode(StepPin, OUTPUT); //Step
+pinMode(DirectionPin, OUTPUT); //Direction
+if (FrontPin != -1) {
+pinMode(FrontPin, INPUT);
+};
+if (BackPin != -1) {
+pinMode(BackPin, INPUT);
+};
+digitalWrite(EnablePin, LOW); //Turn on voltage
+}
+
+void putEE(int eeAddress, int CalValue)
+{
+EEPROM.put(eeAddress, CalValue);
+}
+
+int getEE(int eeAddress)
+{
+int MaxTurn;
+EEPROM.get(eeAddress, MaxTurn);
+return MaxTurn;
+}
+
+int getFrontPinVal()
+{
+if (FrontPin == -1)
+{
+return -1;
+}
+else
+{
+return digitalRead(FrontPin);
+}
+}
+
+int getBackPinVal()
+{
+if (BackPin == -1)
+{
+return -1;
+}
+else
+{
+return digitalRead(BackPin);
+}
+}
+
+int stopMovee() //Возвращает 1 если закрыт Front концевик, 2 если закрыт Back концевик, 0 если остановка не нужна
+{
+if ( FrontPinMode != -1 && this->getFrontPinVal() != FrontPinMode ) //Если режим датчиков не установлен, то считается, что их нет
+{
+return 1;
+}
+if ( BackPinMode != -1 && this->getBackPinVal() != BackPinMode )
+{
+return 2;
+}
+return 0;
+}
+
+// Do move while engine don't come to front end
+void moveToFront(int MinStep, int speedDiv)
+{
+while ( this->stopMovee() != 1 )
+{
+if (stepEngine == 0)
+{
+this->movee(MinStep, speedDiv);
+}
+}
+TurnMax = turn;
+}
+
+// Do move while engine don't come to back end
+void moveToBack(int MinStep, int speedDiv)
+{
+while ( this->stopMovee() != 2 )
+{
+if (stepEngine == 0)
+{
+this->movee(-MinStep, speedDiv);
+}
+}
+turn = 0;
+}
+
+void Calibration(int MinStep, int speedDiv)
+{
+this->moveToBack(MinStep, speedDiv);
+this->moveToFront(MinStep, speedDiv);
+putEE(eeAddressPiston, TurnMax);
+}
+
+// Return speed divider
+unsigned int getSpeedE()
+{
+return speedDivider;
+}
+
+// Return angle
+int getStep()
+{
+return stepEngine;
+}
+
+// Do one step in one side(? degrees)
+void dicStepEngine()
+{
+stepEngine -= 1;
+turn += 1;
+}
+
+// Do one step in other side(? degrees )
+void incStepEngine()
+{
+stepEngine += 1;
+turn -= 1;
+}
+
+// Set angle and speed of move and start
+void movee(int stepEn, unsigned int sd)
+{
+stepEngine = 2 * stepEn;
+speedDivider = sd;
+sD = 2 * abs(stepEngine) * speedDivider;
+}
+
+void engineStepPos()
+{
+int checkMovee = this->stopMovee();
+if (checkMovee != 1) {
+switcher = !switcher;
+digitalWrite(DirectionPin, HIGH);
+digitalWrite(StepPin, switcher); // First - low, following - high - /is it normal?/ In first interrupt sD = 1, and it's normal. When we have even number, last bit in sD ever is 0
+this->dicStepEngine();
+}
+else
+{
+this->movee(0, 0);
+this->resetSD();
+}
+}
+
+void engineStepNeg()
+{
+int checkMovee = this->stopMovee();
+if (checkMovee != 2) {
+switcher = !switcher;
+digitalWrite(DirectionPin, LOW);
+digitalWrite(StepPin, switcher); // First - low, following - high - /is it normal?/ In first interrupt sD = 1, and it's normal. When we have even number, last bit
+in sD ever is 0
+this->incStepEngine();
+}
+else
+{
+this->movee(0, 0);
+this->resetSD();
+}
+}
+
+void dicSD()
+{
+sD--;
+}
+
+unsigned int getSD()
+{
+return sD;
+}
+
+// Reset speedDivider
+void resetSD()
+{
+speedDivider = 0;
+sD = 0;
+}
+
+int getTurnMax()
+{
+return TurnMax;
+}
+
+void interr()
+{
+if ( getStep() != 0 )
+{
+dicSD(); //Counter for speed divider
+}
+
+// In one side
+if ( getStep() > 0 && (getSD() % (2 * getSpeedE()) == 0) )
+{
+engineStepPos(); // Move in positive direction
+}
+
+// In other side
+if ( getStep() < 0 && (getSD() % (2 * getSpeedE()) == 0) )
+{
+engineStepNeg(); // Move in negative direction
+}
+
+if ( !getSD() )
+{
+resetSD();
+}
+}
+protected:
+
+int turn;
+int stepEngine;
+unsigned int speedDivider;
+bool switcher;
+int TurnMax;
+int TurnMin;
+
+//Pins installization
+int StepPin;
+int EnablePin;
+int DirectionPin;
+int FrontPin;
+int BackPin;
+int FrontPinMode;
+int BackPinMode;
+unsigned int sD ;
+};
+
+
+// Object installization. Set number of pins (StepPin, EnablePin, DirectionPin)
+engine piston(PISTON_STEP_PIN, PISTON_ENABLE_PIN, PISTON_DIR_PIN, A0, A1, 0, 0);
+engine fin();
+engine mass(MASS_STEP_PIN, MASS_ENABLE_PIN, MASS_DIR_PIN, A2, A3, 0, 0);
+
+
+void setup()
+{
+Serial.begin(9600);
+piston.installization();
+mass.installization();
+OCR0A = 0xAF; // If we want max speed, we can reduce the timer time by 2 times, because we increased the number of interrupts by 2 times
+TIMSK0 |= _BV(OCIE0A);
+piston.Calibration(10, 1);
+mass.Calibration(10, 1);
+}
+
+
+SIGNAL(TIMER0_COMPA_vect)
+{
+piston.interr();
+mass.interr();
+}
+
+void loop()
+{
+maxTurn1 = piston.getEE(eeAddressPiston);
+maxTurn2 = mass.getEE(eeAddressMass);
+piston.movee(200, 2);
+mass.movee(-200, 1);
+delay(2000);
+piston.movee(-200, 1);
+mass.movee(200, 2);
+delay(2000);
+}
